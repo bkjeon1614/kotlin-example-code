66005e6383e094ca87f09ec0fb95e8a399ff78ec
bongkeun joen <gcijdfdo@gmail.com>
2023-12-19 01:02:42 +0900

    코틀린 19강할차례

--- java-converting-kotlin/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ java-converting-kotlin/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -24,7 +24,7 @@ class UserService(
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse> {
         return userRepository.findAll()
-            .map { user -> UserResponse(user) }
+            .map { user -> UserResponse.of(user) }
             // or: .map { UserResponse(it) }
             // or: ::UserResponse
             // map 을 쓰는걸 제일 선호
