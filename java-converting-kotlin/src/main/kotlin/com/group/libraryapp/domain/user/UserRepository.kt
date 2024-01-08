e2da816bbfa47c33f12109a8d0eabb820972fb55
bongkeun joen <gcijdfdo@gmail.com>
2024-01-08 21:47:06 +0900

    30강 들을차례

--- java-converting-kotlin/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ java-converting-kotlin/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,9 +1,14 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long> {
 
     fun findByName(nam: String): User?
+
+    // JPQL 사용 (이것보다 QueryDSL 을 사용하는게 더 편하다)
+    @Query("SELECT DISTINCT u FROM User u LEFT JOIN u.userLoanHistories")
+    fun findAllWithHistories(): List<User>
 
 }
