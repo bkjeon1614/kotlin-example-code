473708708e68ef03b76e08b2f7e92a503287491d
전봉근(롯데쇼핑 e커머스-파인딩플랫폼개발팀) <gcijdfdo@gmail.com>
2024-06-17 18:34:40 +0900

    kotest 리팩토링

--- kotlin-jpa-codebase/src/test/kotlin/com/bkjeon/base/v1/api/elasticsearch/service/ElasticsearchServiceTest.kt
+++ kotlin-jpa-codebase/src/test/kotlin/com/bkjeon/base/v1/api/elasticsearch/service/ElasticsearchServiceTest.kt
@@ -13,49 +13,40 @@ internal class ElasticsearchServiceTest(
     val esSampleRepository: ElasticsearchSampleRepository
 ): BehaviorSpec({
 
+    val request = EsRequest(
+        startDate = null,
+        endDate = null,
+        title = null,
+        author = null,
+        age = null,
+        ageNosStr = "20"
+    )
+
     given("ElasticsearchSampleRepository 가 있고") {
         val elasticsearchService = ElasticsearchService(esSampleRepository)
 
         When("getSampleAggsList 를 불러오면") {
             val sampleAggsList = elasticsearchService.getSampleAggsList()
 
-            then("null 값이 아니고, 0 보다 크다.") {
+            Then("null 값이 아니고, 0 보다 크다.") {
                 sampleAggsList shouldNotBe null
                 sampleAggsList.size shouldNotBe 0
             }
         }
 
         When("getSampleList 를 불러오면") {
-            val sampleList = elasticsearchService.getSampleList(
-                request = EsRequest(
-                    startDate = null,
-                    endDate = null,
-                    title = null,
-                    author = null,
-                    age = null,
-                    ageNosStr = "20"
-                )
-            )
+            val sampleList = elasticsearchService.getSampleList(request)
 
-            then("null 값이 아니고, 0 보다 크다.") {
+            Then("null 값이 아니고, 0 보다 크다.") {
                 sampleList.list shouldNotBe null
                 sampleList.list.size shouldNotBe 0
             }
         }
 
         When("getSampleTotalCnt 를 불러오면") {
-            val sampleTotalCnt = elasticsearchService.getSampleTotalCnt(
-                request = EsRequest(
-                    startDate = null,
-                    endDate = null,
-                    title = null,
-                    author = null,
-                    age = null,
-                    ageNosStr = "20"
-                )
-            )
+            val sampleTotalCnt = elasticsearchService.getSampleTotalCnt(request)
 
-            then("sampleTotalCnt should not be 0") {
+            Then("sampleTotalCnt should not be 0") {
                 sampleTotalCnt shouldNotBe 0
             }
         }
